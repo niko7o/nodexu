30ee359a12c3a5fb08e47ef05bb79f1ed757cd04
Nikolaas <nikolaas.verlee@idreal.com>
2018-07-24 10:22:49 +0200

    User status HBS helper done

---
+++ helpers/user.helpers.js
@@ -0,0 +1,9 @@
+module.exports = (hbs) => {
+    hbs.registerHelper('get_user_status', (user) => {
+        if (user.active) {
+            return 'active';
+        } else {
+            return 'danger';
+        }
+    });
+}
