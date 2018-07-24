6751cc137d49d79831c182b028244939bb0426f5
Nikolaas <nikolaas.verlee@idreal.com>
2018-07-24 10:36:12 +0200

    Added badge functionality for user views

--- configs/hbs.config.js
+++ configs/hbs.config.js
@@ -2,5 +2,6 @@ const hbs = require('hbs');
 const path = require('path');
 
 require('../helpers/misc.helpers')(hbs);
+require('../helpers/user.helpers')(hbs);
 
 hbs.registerPartials(path.join(__dirname, '../views'));
