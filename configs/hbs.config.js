e0faab2eeb2623c097e7677b32db5fad20e939f7
Nikolaas <nikolaas.verlee@idreal.com>
2018-07-26 10:18:18 +0200

    Map is loading with api key

--- configs/hbs.config.js
+++ configs/hbs.config.js
@@ -4,4 +4,4 @@ const path = require('path');
 require('../helpers/misc.helpers')(hbs);
 require('../helpers/user.helpers')(hbs);
 
-hbs.registerPartials(path.join(__dirname, '../views'));
+hbs.registerPartials(path.join(__dirname, '../views/partials'));
